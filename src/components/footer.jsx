92dcb0be03e62b871877f62d2bc76d6abe48738e
Aidan Sheehan <aidansheehan@Aidans-MacBook-Air.local>
2023-04-13 08:27:39 +0100

    footer new: implemented footer

---
+++ src/components/footer.jsx
@@ -0,0 +1,13 @@
+/**
+ * Footer component
+ */
+const FooterComponent = () => {
+
+    return (
+        <div className='mt-2 py-4 w-full text-center text-accent opacity-50' >
+            © Aidan Sheehan 2023
+        </div>
+    )
+}
+
+export default FooterComponent;
